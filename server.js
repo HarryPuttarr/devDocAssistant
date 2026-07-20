aeae2fb5dd180cb7f289c19913452a73c00bbe58
HarryPuttarr <harshit26964@gmail.com>
2026-07-20 15:13:11 +0530

    Connect frontend to live Render backend API bug fixed

--- server.js
+++ server.js
@@ -32,7 +32,6 @@ if (missingVars.length > 0) {
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
 // --- Middleware -------------------------------------------------------
