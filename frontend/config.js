9d12c47364a8a588686d83d0be18b8ec1e248e48
HarryPuttarr <harshit26964@gmail.com>
2026-07-20 14:59:26 +0530

    Connect frontend to live Render backend API

--- frontend/config.js
+++ frontend/config.js
@@ -12,4 +12,4 @@
  * --------------------------------------------------------------------------
  */
 
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'https://devdocassistant.onrender.com';
